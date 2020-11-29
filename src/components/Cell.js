f2875f09dbfe4b114ed1bdeaf950f79eee902ccc
👻 <>
2020-11-29 13:25:18 +0800

    add display file

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { StyledCell } from './styles/StyledCell';
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
 
 const Cell = ({ value }) => (
-<StyledCell value={value}><span>{value === 0 ? '' : value }</span></ StyledCell>
-)
+  <StyledCell value={value}>
+    <span>{value === 0 ? "" : value}</span>
+  </StyledCell>
+);
 
 export default Cell;
